fda4c6d82af3b192016aeb3305434ffd5148d0b2
masa-1016 <77272798+masa-1016@users.noreply.github.com>
2021-06-05 21:19:01 +0900

    room_usersテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,5 +4,8 @@ class Room < ApplicationRecord
   belongs_to_active_hash :game
   belongs_to_active_hash :platform
 
-  validates :game_id, :platform_id, numericality: { other_than: 1 } 
+  validates :game_id, :platform_id, numericality: { other_than: 1 }
+
+  has_many :room_users
+  has_many :users, through: :room_users
 end
