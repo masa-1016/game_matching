c0473fb897767f7d1e79f056b6d86c03b56658dd
masa-1016 <77272798+masa-1016@users.noreply.github.com>
2021-06-07 21:46:33 +0900

    チャットルームの削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -7,7 +7,7 @@ class Room < ApplicationRecord
   validates :name, presence: true
   validates :game_id, :platform_id, numericality: { other_than: 1 }
 
-  has_many :room_users
+  has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users
-  has_many :messages
+  has_many :messages, dependent: :destroy
 end
